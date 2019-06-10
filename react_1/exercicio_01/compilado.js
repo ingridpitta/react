270df406732237305f72dc0d4bdefc6d278c4d74
ingridpitta <ingridpitta056@gmail.com>
2019-06-10 17:20:37 -0300

    first commit

---
+++ react_1/exercicio_01/compilado.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var app = document.getElementById("app"); // let elemento = React.createElement("h1", null, "React na Reprograma, na segunda-feira");
+
+var teste = React.createElement("h2", null, "Agora vai, chibataaaaa!"); //Jsx;
+
+ReactDOM.render(teste, app); //Primeiro o elemento depois o local;
---
+++ react_1/exercicio_01/script.js
@@ -0,0 +1,4 @@
+let app = document.getElementById("app")
+// let elemento = React.createElement("h1", null, "React na Reprograma, na segunda-feira");
+let teste=<h2>Reprograma!</h2>; //Jsx;
+ReactDOM.render(teste,app) //Primeiro o elemento depois o local;
